15fc1446bb4add3e2ebc1d241b261506fac531f4
Marti Bolivar <marti@opensourcefoundries.com>
2018-06-04 12:04:47 -0400

    Add setuptools integration.
    Follow along with the tutorial:
    
    https://packaging.python.org/tutorials/packaging-projects/
    
    The version number is because of repeated uploads to TestPyPI.
    
    Signed-off-by: Marti Bolivar <marti@opensourcefoundries.com>

---
+++ README.rst
@@ -0,0 +1,13 @@
+This package contains the Zephyr RTOS meta tool, 'west'.
+
+To install west from this package into your user directory, run:
+
+$ python3 setup.py install --user
+
+You can also try it out by just running the package as a module from
+this directory:
+
+$ python3 -m west
+
+If you go this route, make sure to install the requirements as
+specified by the install_requires line in setup.py.
---
+++ setup.py
@@ -0,0 +1,37 @@
+# Copyright 2018 Open Source Foundries Limited.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import setuptools
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='west',
+    version='0.1.10',
+    author='Zephyr Project',
+    author_email='devel@lists.zephyrproject.org',
+    description='Zephyr RTOS Project meta-tool',
+    long_description=long_description,
+    # http://docutils.sourceforge.net/FAQ.html#what-s-the-official-mime-type-for-restructuredtext-data
+    long_description_content_type="text/x-rst",
+    url='https://github.com/zephyrproject-rtos/west',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        ),
+    install_requires=(
+        'PyYAML',
+        ),
+    python_requires='>=3.4',
+    entry_points={
+        'console_scripts': (
+            'west = west.main:main',
+            ),
+        },
+    )
