3cb13ab2dff1bd8490828ce84bf88473ec0c975f
Marti Bolivar <marti@foundries.io>
2018-09-24 12:51:09 -0600

    west.util: generalize top-level directory detection
    Allow finding the top level west install directory from any starting
    point on the system with a new start kwarg in west_dir() and
    west_topdir().
    
    Add a new convenience predicate, in_multirepo_install(), for simply
    checking whether or not a starting point is part of a multi-repo west
    installation.
    
    Signed-off-by: Marti Bolivar <marti@foundries.io>

--- src/west/util.py
+++ src/west/util.py
@@ -27,17 +27,19 @@ class WestNotFound(RuntimeError):
     '''Neither the current directory nor any parent has a West installation.'''
 
 
-def west_dir():
+def west_dir(start=None):
+    '''Returns the absolute path of the west/ top level directory.
+
+    Starts the search from the start directory, and goes to its
+    parents. If the start directory is not specified, the current
+    directory is used.
+
+    Raises WestNotFound if no west top-level directory is found.
     '''
-    Returns the absolute path of the first west/ directory found, searching the
-    current directory and its parents.
-
-    Raises WestNotFound if no west/ directory is found.
-    '''
-    return os.path.join(west_topdir(), 'west')
+    return os.path.join(west_topdir(start), 'west')
 
 
-def west_topdir():
+def west_topdir(start=None):
     '''
     Like west_dir(), but returns the path to the parent directory of the west/
     directory instead, where project repositories are stored
@@ -45,7 +47,10 @@ def west_topdir():
     # If you change this function, make sure to update the bootstrap
     # script's find_west_topdir().
 
-    cur_dir = os.getcwd()
+    if start is None:
+        cur_dir = os.getcwd()
+    else:
+        cur_dir = start
 
     while True:
         if os.path.isfile(os.path.join(cur_dir, 'west', '.west_topdir')):
@@ -57,3 +62,19 @@ def west_topdir():
             raise WestNotFound('Could not find a West installation '
                                'in this or any parent directory')
         cur_dir = parent_dir
+
+
+def in_multirepo_install(start=None):
+    '''Returns True iff the path ``start`` is in a multi-repo installation.
+
+    If start is not given, it defaults to the current working directory.
+
+    This is equivalent to checking if west_dir() raises an exception
+    when given the same start kwarg.
+    '''
+    try:
+        west_topdir(start)
+        result = True
+    except WestNotFound:
+        result = False
+    return result
