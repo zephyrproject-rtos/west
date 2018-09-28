d1a24c0be042d460a1e98c9db2964b9f3a9c0dbc
Marti Bolivar <marti@foundries.io>
2018-09-28 13:38:12 -0600

    Reimplement west.manifest with more complete abstract types
    Currently, west.manifest mostly can just return a list of Project
    types, each of which is a namedtuple for the project contents we care
    about.
    
    It would be better and more flexible to have a "real" data type for
    the manifest, along with concrete representations for its contents
    such as default values and remotes.
    
    Reimplement the module to add just these things, adding Manifest,
    Defaults, and Remote types, and re-working the Project namedtuple into
    a class type. All of these (except Manifest) use __slots__ to restrict
    their attributes.
    
    Keep the current users and test cases up to date, extending the tests
    a bit more as well. This moves a bit more code out of project.py into
    west.manifest.
    
    Signed-off-by: Marti Bolivar <marti@foundries.io>

--- src/west/manifest.py
+++ src/west/manifest.py
@@ -3,102 +3,286 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import collections
+'''Parser and abstract data types for west manifests.
+
+The main class is Manifest. The recommended method for creating a
+Manifest instance is via its from_file() or from_data() helper
+methods.
+
+There are additionally Defaults, Remote, and Project types defined,
+which represent the values by the same names in a west
+manifest. (I.e. "Remote" represents one of the elements in the
+"remote" sequence in the manifest, and so on.) Some Default values,
+such as the default project revision, may be supplied by this module
+if they are not present in the manifest data.'''
+
 import os
 
 import pykwalify.core
 import yaml
 
-from west import util
-
-Project = collections.namedtuple(
-    'Project',
-    'name url revision path abspath clone_depth')
-'''Holds information about a project, taken from the manifest file.'''
+from west import util, log
 
 
-class MalformedManifest(RuntimeError):
-    pass
+def default_path():
+    '''Return the path to the default manifest in the west directory.
+
+    Raises WestNotFound if called from outside of a west working directory.'''
+    return os.path.join(util.west_dir(), 'manifest', 'default.yml')
 
 
-def manifest_projects(manifest_path):
-    '''Return a list of Project instances for the given manifest path.'''
-    def _malformed_manifest(msg):
-        _malformed(manifest_path, msg)
+class Manifest:
+    '''Represents the contents of a West manifest file.
 
-    manifest = _validated_manifest(manifest_path)
+    The most convenient way to construct an instance is using the
+    from_file and from_data helper methods.'''
 
-    projects = []
+    @staticmethod
+    def from_file(source_file=None):
+        '''Create and return a new Manifest object given a source YAML file.
 
-    # Get the defaults object out of the manifest, or provide one.
-    defaults = manifest.get('defaults', {'revision': 'master'})
-    default_rev = defaults.get('revision')
-    default_remote = defaults.get('remote')
+        :param source_file: Path to a YAML file containing the manifest.
 
-    # Map from each remote's name onto that remote's data in the manifest.
-    remote_dict = {r['name']: r for r in manifest['remotes']}
+        If source_file is None, the value returned by default_path()
+        is used.
 
-    # The default remote, if specified, must be defined.
-    if default_remote is not None and default_remote not in remote_dict:
-        _malformed_manifest('default remote {} is not defined'.
-                            format(default_remote))
+        Raises MalformedManifest in case of validation errors.'''
+        if source_file is None:
+            source_file = default_path()
+        return Manifest(source_file=source_file)
 
-    # mp = manifest project (dictionary with values parsed from the manifest)
-    for mp in manifest['projects']:
-        # Validate the project remote.
-        mpr = mp.get('remote') or defaults.get('remote')
-        if mpr is None:
-            _malformed_manifest('project {} does not specify a remote'.
+    @staticmethod
+    def from_data(source_data):
+        '''Create and return a new Manifest object given parsed YAML data.
+
+        :param source_data: Parsed YAML data as a Python object.
+
+        Raises MalformedManifest in case of validation errors.'''
+        return Manifest(source_data=source_data)
+
+    def __init__(self, source_file=None, source_data=None):
+        '''Create a new Manifest object.
+
+        :param source_file: Path to a YAML file containing the manifest.
+        :param source_data: Parsed YAML data as a Python object.
+
+        Normally, it is more convenient to use the `from_file` and
+        `from_data` convenience factories than calling the constructor
+        directly.
+
+        Exactly one of the source_file and source_data parameters must
+        be given.
+
+        Raises MalformedManifest in case of validation errors.'''
+        if source_file and source_data:
+            raise ValueError('both source_file and source_data were given')
+
+        if source_file:
+            with open(source_file, 'r') as f:
+                self._data = yaml.safe_load(f.read())
+            path = source_file
+        else:
+            self._data = source_data
+            path = None
+
+        self.path = path
+        '''Path to the file containing the manifest, or None if created
+        from data rather than the file system.'''
+
+        if not self._data:
+            self._malformed('manifest contains no data')
+
+        try:
+            pykwalify.core.Core(
+                source_data=self._data,
+                schema_files=[_SCHEMA_PATH]
+            ).validate()
+        except pykwalify.errors.SchemaError as e:
+            self._malformed(e)
+
+        self.defaults = None
+        '''west.manifest.Defaults object representing default values
+        in the manifest, either as specified by the user or west itself.'''
+
+        self.remotes = None
+        '''Sequence of west.manifest.Remote objects representing manifest
+        remotes.'''
+
+        self.projects = None
+        '''Sequence of west.manifest.Project objects representing manifest
+        projects.
+
+        Each element's values are fully initialized; there is no need
+        to consult the defaults field to supply missing values.'''
+
+        # Set up the public attributes documented above, as well as
+        # any internal attributes needed to implement the public API.
+        self._load(self._data['manifest'])
+
+    def get_remote(self, name):
+        '''Get a manifest Remote, given its name.'''
+        return self._remotes_dict[name]
+
+    def _malformed(self, complaint):
+        context = (' file {} '.format(self.path) if self.path
+                   else ' data:\n{}\n'.format(self._data))
+        raise MalformedManifest('Malformed manifest{}(schema: {}):\n{}'
+                                .format(context, _SCHEMA_PATH, complaint))
+
+    def _load(self, manifest):
+        # Initialize this instance's fields from values given in the
+        # manifest data, which must be validated according to the schema.
+
+        projects = []
+
+        # Map from each remote's name onto that remote's data in the manifest.
+        remotes = tuple(Remote(r['name'], r['url']) for r in
+                        manifest['remotes'])
+        remotes_dict = {r.name: r for r in remotes}
+
+        # Get any defaults out of the manifest.
+        #
+        # md = manifest defaults (dictionary with values parsed from
+        # the manifest)
+        md = manifest.get('defaults', dict())
+        mdrem = md.get('remote')
+        if mdrem:
+            # The default remote name, if provided, must refer to a
+            # well-defined remote.
+            if mdrem not in remotes_dict:
+                self._malformed('default remote {} is not defined'.
+                                format(mdrem))
+            default_remote = remotes_dict[mdrem]
+            default_remote_name = mdrem
+        else:
+            default_remote = None
+            default_remote_name = None
+        defaults = Defaults(remote=default_remote, revision=md.get('revision'))
+
+        # mp = manifest project (dictionary with values parsed from
+        # the manifest)
+        for mp in manifest['projects']:
+            # Validate the project remote.
+            remote_name = mp.get('remote', default_remote_name)
+            if remote_name is None:
+                self._malformed('project {} does not specify a remote'.
                                 format(mp['name']))
-        if mpr not in remote_dict:
-            _malformed_manifest('project {} remote {} is not defined'.
-                                format(mp['name'], mp['remote']))
+            if remote_name not in remotes_dict:
+                self._malformed('project {} remote {} is not defined'.
+                                format(mp['name'], remote_name))
+            projects.append(Project(mp['name'],
+                                    remotes_dict[remote_name],
+                                    defaults,
+                                    path=mp.get('path'),
+                                    clone_depth=mp.get('clone-depth'),
+                                    revision=mp.get('revision')))
 
-        # If no clone path is specified, the project's name is used
-        clone_path = mp.get('path', mp['name'])
-
-        # Use named tuples to store project information. That gives nicer
-        # syntax compared to a dict (project.name instead of project['name'],
-        # etc.)
-        projects.append(Project(
-            mp['name'],
-            # The project repository URL is formed by concatenating the
-            # remote URL with the project name.
-            remote_dict[mpr]['url'] + '/' + mp['name'],
-            # The project revision is defined in its entry or given by a
-            # default value.
-            mp.get('revision', default_rev),
-            clone_path,
-            # Absolute clone path
-            os.path.join(util.west_topdir(), clone_path),
-            # If no clone depth is specified, we fetch the entire history
-            mp.get('clone-depth', None)))
-
-    return projects
+        self.defaults = defaults
+        self.remotes = remotes
+        self._remotes_dict = remotes_dict
+        self.projects = tuple(projects)
 
 
-def _validated_manifest(manifest_path):
-    # Validates the manifest with pykwalify.
-    with open(manifest_path, 'r') as f:
-        manifest_data = yaml.safe_load(f.read())
-
-    if not manifest_data:
-        _malformed(manifest_path, 'No YAML content was found.')
-
-    try:
-        pykwalify.core.Core(
-            source_data=manifest_data,
-            schema_files=[_SCHEMA_PATH]
-        ).validate()
-    except pykwalify.errors.SchemaError as e:
-        _malformed(manifest_path, e)
-
-    return manifest_data['manifest']
+class MalformedManifest(Exception):
+    '''Exception indicating that west manifest parsing failed due to a
+    malformed value.'''
 
 
-def _malformed(manifest_path, error_info):
-    raise MalformedManifest('{} malformed (schema: {}):\n{}'
-                            .format(manifest_path, _SCHEMA_PATH, error_info))
+# Definitions for Manifest attribute types.
+
+class Defaults:
+    '''Represents default values in a manifest, either specified by the
+    user or by west itself.
+
+    Defaults are neither comparable nor hashable.'''
+
+    __slots__ = 'remote revision'.split()
+
+    def __init__(self, remote=None, revision=None):
+        if remote is not None:
+            _wrn_if_not_remote(remote)
+        if revision is None:
+            revision = 'master'
+
+        self.remote = remote
+        self.revision = revision
+
+    def __eq__(self, other):
+        raise NotImplemented
+
+    def __repr__(self):
+        return 'Defaults(remote={}, revision={})'.format(repr(self.remote),
+                                                         repr(self.revision))
+
+
+class Remote:
+    '''Represents a remote defined in a west manifest.
+
+    Remotes may be compared for equality, but are not hashable.'''
+
+    __slots__ = 'name url'.split()
+
+    def __init__(self, name, url):
+        if url.endswith('/'):
+            log.wrn('Remote', name, 'URL', url, 'ends with a slash ("/");',
+                    'these are automatically appended by West')
+
+        self.name = name
+        self.url = url
+
+    def __eq__(self, other):
+        return self.name == other.name and self.url == other.url
+
+    def __repr__(self):
+        return 'Remote(name={}, url={})'.format(repr(self.name),
+                                                repr(self.url))
+
+
+class Project:
+    '''Represents a project defined in a west manifest.
+
+    Projects are neither comparable nor hashable.'''
+
+    __slots__ = 'name remote url path abspath clone_depth revision'.split()
+
+    def __init__(self, name, remote, defaults, path=None, clone_depth=None,
+                 revision=None):
+        '''Specify a Project by name, Remote, and optional information.
+
+        :param name: Project's user-defined name in the manifest
+        :param remote: Remote instance corresponding to this Project's remote.
+                       This may not be None.
+        :param path: Relative path to the project in the west
+                     installation, if present in the manifest. If None,
+                     the project's ``name`` is used.
+        :param revision: Project revision as given in the manifest, if present.
+        '''
+        if remote is None:
+            raise ValueError('remote may not be None')
+        _wrn_if_not_remote(remote)
+
+        self.name = name
+        self.remote = remote
+        self.url = remote.url + '/' + name
+        self.path = path or name
+        self.abspath = os.path.join(util.west_topdir(), self.path)
+        self.clone_depth = clone_depth
+        self.revision = revision or defaults.revision
+
+    def __eq__(self, other):
+        raise NotImplemented
+
+    def __repr__(self):
+        reprs = [repr(x) for x in
+                 (self.name, self.remote, self.url, self.path,
+                  self.abspath, self.clone_depth, self.revision)]
+        return ('Project(name={}, remote={}, url={}, path={}, abspath={}, '
+                'clone_depth={}, revision={})').format(*reprs)
+
+
+def _wrn_if_not_remote(remote):
+    if not isinstance(remote, Remote):
+        log.wrn('Remote', remote, 'is not a Remote instance')
 
 
 _SCHEMA_PATH = os.path.join(os.path.dirname(__file__), "manifest-schema.yml")
--- tests/west/manifest/test_manifest.py
+++ tests/west/manifest/test_manifest.py
@@ -7,22 +7,27 @@ import os
 from unittest.mock import patch
 
 import pytest
+import yaml
 
-from west.manifest import manifest_projects, Project, MalformedManifest
+from west.manifest import Manifest, Defaults, Remote, Project, \
+    MalformedManifest
 
 THIS_DIRECTORY = os.path.dirname(__file__)
 
 
-def check_projects(tmpdir, content, expected):
-    with patch('west.util.west_topdir', return_value='/west_top'):
-        content_yml = str(tmpdir.join('content.yml'))
-        with open(content_yml, 'w') as f:
-            f.write(content)
-        projects = manifest_projects(content_yml)
-        assert projects == expected
+def deep_eq_check(actual, expected):
+    # Check equality of all project fields (projects themselves are
+    # not comparable).
+    assert actual.name == expected.name
+    assert actual.remote == expected.remote
+    assert actual.url == expected.url
+    assert actual.path == expected.path
+    assert actual.abspath == expected.abspath
+    assert actual.clone_depth == expected.clone_depth
+    assert actual.revision == expected.revision
 
 
-def test_no_defaults(tmpdir):
+def test_no_defaults():
     # Manifests with no defaults should work.
     content = '''\
     manifest:
@@ -39,24 +44,27 @@ def test_no_defaults(tmpdir):
         - name: testproject2
           remote: testremote2
     '''
-    expected = [
-        Project('testproject1',
-                'https://example1.com/testproject1',
-                'rev1',
-                'testproject1',
-                '/west_top/testproject1',
-                None),
-        Project('testproject2',
-                'https://example2.com/testproject2',
-                'master',
-                'testproject2',
-                '/west_top/testproject2',
-                None),
-    ]
-    check_projects(tmpdir, content, expected)
+    r1 = Remote('testremote1', 'https://example1.com')
+    r2 = Remote('testremote2', 'https://example2.com')
+
+    with patch('west.util.west_topdir', return_value='/west_top'):
+        manifest = Manifest.from_data(yaml.safe_load(content))
+
+        expected = [Project('testproject1', r1, None, path='testproject1',
+                            clone_depth=None, revision='rev1'),
+                    Project('testproject2', r2, None, path='testproject2',
+                            clone_depth=None, revision='master')]
+
+    # Check the remotes are as expected.
+    assert list(manifest.remotes) == [r1, r2]
+
+    # Check the projects are as expected.
+    for p, e in zip(manifest.projects, expected):
+        deep_eq_check(p, e)
+    assert all(p.abspath == '/west_top/' + p.path for p in manifest.projects)
 
 
-def test_default_clone_depth(tmpdir):
+def test_default_clone_depth():
     # Defaults and clone depth should work as in this example.
     content = '''\
     manifest:
@@ -77,26 +85,53 @@ def test_default_clone_depth(tmpdir):
           revision: rev
           clone-depth: 1
     '''
-    expected = [
-        Project('testproject1',
-                'https://example1.com/testproject1',
-                'defaultrev',
-                'testproject1',
-                '/west_top/testproject1',
-                None),
-        Project('testproject2',
-                'https://example2.com/testproject2',
-                'rev',
-                'testproject2',
-                '/west_top/testproject2',
-                1),
-    ]
-    check_projects(tmpdir, content, expected)
+    r1 = Remote('testremote1', 'https://example1.com')
+    r2 = Remote('testremote2', 'https://example2.com')
+    d = Defaults(remote=r1, revision='defaultrev')
+
+    with patch('west.util.west_topdir', return_value='/west_top'):
+        manifest = Manifest.from_data(yaml.safe_load(content))
+
+        expected = [Project('testproject1', r1, d, path='testproject1',
+                            clone_depth=None, revision=d.revision),
+                    Project('testproject2', r2, d, path='testproject2',
+                            clone_depth=1, revision='rev')]
+
+    # Check that default attributes match.
+    assert manifest.defaults.remote == d.remote
+    assert manifest.defaults.revision == d.revision
+
+    # Check the remotes are as expected.
+    assert list(manifest.remotes) == [r1, r2]
+
+    # Check that the projects are as expected.
+    for p, e in zip(manifest.projects, expected):
+        deep_eq_check(p, e)
+    assert all(p.abspath == '/west_top/' + p.path for p in manifest.projects)
+
+
+def test_path():
+    # Projects must be able to override their default paths.
+    content = '''\
+    manifest:
+      remotes:
+        - name: testremote
+          url: https://example.com
+      projects:
+        - name: testproject
+          remote: testremote
+          path: sub/directory
+    '''
+    with patch('west.util.west_topdir', return_value='/west_top'):
+        manifest = Manifest.from_data(yaml.safe_load(content))
+    assert manifest.projects[0].path == 'sub/directory'
+    assert manifest.projects[0].abspath == '/west_top/sub/directory'
 
 
 # Invalid manifests should raise MalformedManifest.
 @pytest.mark.parametrize('invalid',
                          glob(os.path.join(THIS_DIRECTORY, 'invalid_*.yml')))
-def test_invalid(invalid):
+@patch('west.util.west_topdir', return_value='/west_top')
+def test_invalid(topdir, invalid):
     with pytest.raises(MalformedManifest):
-        manifest_projects(invalid)
+        Manifest.from_file(invalid)
