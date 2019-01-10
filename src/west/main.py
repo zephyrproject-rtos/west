f77b0bab0cf431e3931c11ef0111422d5fe6cc18
Marti Bolivar <marti@foundries.io>
2019-01-10 10:59:40 -0700

    commands: group related commands together
    Some utilities which use subcommands break them into groups in their
    help output, to make it easier for the reader to find what they are
    looking for. Two notable examples are docker and git.
    
    Do the same thing for west, by tweaking the COMMANDS type to group
    related commands together under a string key which describes the
    group, and adding a custom argument parser subclass which uses this
    information when formatting the top-level command help.
    
    Add a group of "hidden" commands which is just PostInit for
    now. Eventually that will be going away, and in the meantime, we'd
    like to sweep it under the rug.
    
    I would have preferred to do this by hooking into argparse itself, but
    it is annoyingly opaque about what constitutes stable API.
    
    The top-level "west -h" output now looks like this (without ">"s):
    
    > $ west -h
    > usage: west [-h] [-z ZEPHYR_BASE] [-v] [-V] <command> ...
    >
    > The Zephyr RTOS meta-tool.
    >
    > optional arguments:
    >   -h, --help            show this help message and exit
    >   -z ZEPHYR_BASE, --zephyr-base ZEPHYR_BASE
    >                         Override the Zephyr base directory. The
    >                         default is the manifest project with path
    >                         "zephyr".
    >   -v, --verbose         Display verbose output. May be given multiple
    >                         times to increase verbosity.
    >   -V, --version         print the program version and exit
    >
    > west commands used in various situations:
    >  building and running zephyr:
    >      build:             compile a Zephyr application
    >      flash:             flash and run a binary on a board
    >      debug:             flash and interactively debug a Zephyr
    >                         application
    >      debugserver:       connect to board and launch a debug server
    >      attach:            interactively debug a board
    >
    >  managing multiple repositories in the installation:
    >      list:              print information about projects in the west
    >                         manifest
    >      clone:             clone remote projects into local installation
    >      fetch:             "git fetch" changes from project remotes
    >      pull:              "git pull" changes from project remotes
    >      rebase:            "git rebase" local projects onto manifest
    >                         versions
    >      branch:            create a branch in one or more local projects
    >      checkout:          check out a branch in one or more local
    >                         projects
    >      diff:              "git diff" for one or more projects
    >      status:            "git status" for one or more projects
    >      update:            update the manifest and west repositories
    >      forall:            run a command in one or more local projects
    >
    > Run "west <command> -h" for help on each command.
    
    The command-level help (e.g. "west build -h") is not changed.
    
    Fixes: #107
    Signed-off-by: Marti Bolivar <marti@foundries.io>

--- src/west/main.py
+++ src/west/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # Copyright 2018 Open Source Foundries Limited.
+# Copyright 2019 Foundries.io Limited.
 #
 # SPDX-License-Identifier: Apache-2.0
 
@@ -11,9 +12,13 @@
 import argparse
 import colorama
 from functools import partial
+from io import StringIO
+import itertools
 import os
+import shutil
 import sys
 from subprocess import CalledProcessError, check_output, DEVNULL
+import textwrap
 
 from west import log
 from west import config
@@ -25,44 +30,214 @@ from west.commands.project import List, Clone, Fetch, Pull, Rebase, Branch, \
                              Checkout, Diff, Status, Update, ForAll, \
                              WestUpdated, PostInit
 from west.manifest import Manifest, MalformedConfig
-from west.util import quote_sh_list, in_multirepo_install, west_dir
+from west.util import quote_sh_list, in_multirepo_install
 
 IN_MULTIREPO_INSTALL = in_multirepo_install(os.path.dirname(__file__))
 
-BUILD_FLASH_COMMANDS = [
-    Build(),
-    Flash(),
-    Debug(),
-    DebugServer(),
-    Attach(),
-]
+RUNNER_COMMANDS = {
+    'building and running zephyr': [
+        Build(),
+        Flash(),
+        Debug(),
+        DebugServer(),
+        Attach()
+    ]
+}
 
-PROJECT_COMMANDS = [
-    PostInit(),
-    List(),
-    Clone(),
-    Fetch(),
-    Pull(),
-    Rebase(),
-    Branch(),
-    Checkout(),
-    Diff(),
-    Status(),
-    Update(),
-    ForAll(),
-]
+PROJECT_COMMANDS = {
+    'managing multiple repositories in the installation': [
+        List(),
+        Clone(),
+        Fetch(),
+        Pull(),
+        Rebase(),
+        Branch(),
+        Checkout(),
+        Diff(),
+        Status(),
+        Update(),
+        ForAll()
+    ]
+}
+
+# Commands we don't want to show to the user. For now, this is PostInit.
+HIDDEN_COMMANDS = {None: [PostInit()]}
 
 # Built-in commands in this West. For compatibility with monorepo
 # installations of West within the Zephyr tree, we only expose the
 # project commands if this is a multirepo installation.
-COMMANDS = BUILD_FLASH_COMMANDS
+COMMANDS = dict(RUNNER_COMMANDS)
 
 if IN_MULTIREPO_INSTALL:
-    COMMANDS += PROJECT_COMMANDS
+    COMMANDS.update(PROJECT_COMMANDS)
+    COMMANDS.update(HIDDEN_COMMANDS)
 
 
 class InvalidWestContext(RuntimeError):
     pass
+
+
+class WestHelpAction(argparse.Action):
+
+    def __init__(self, option_strings, dest, **kwargs):
+        kwargs['nargs'] = 0
+        super(WestHelpAction, self).__init__(option_strings, dest,
+                                             **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        parser.print_help(top_level=True)
+        parser.exit()
+
+
+class WestArgumentParser(argparse.ArgumentParser):
+    # The argparse module is infuriatingly coy about its parser and
+    # help formatting APIs, marking almost everything you need to
+    # customize help output an "implementation detail". Even accessing
+    # the parser's description and epilog attributes as we do here is
+    # technically breaking the rules.
+    #
+    # Even though the implementation details have been pretty stable
+    # since the module was first introduced in Python 3.2, let's avoid
+    # possible headaches by overriding some "proper" argparse APIs
+    # here instead of monkey-patching the module or breaking
+    # abstraction barriers. This is duplicative but more future-proof.
+
+    def __init__(self, *args, **kwargs):
+        # The super constructor calls add_argument(), so this has to
+        # come first as our override of that method relies on it.
+        self.west_optionals = []
+
+        super(WestArgumentParser, self).__init__(*args, **kwargs)
+
+    def print_help(self, file=None, top_level=False):
+        print(self.format_help(top_level=top_level),
+              file=file or sys.stdout)
+
+    def format_help(self, top_level=False):
+        # When top_level is True, we override the parent method to
+        # produce more readable output, which separates commands into
+        # logical groups. In order to print optionals, we rely on the
+        # data available in our add_argument() override below.
+        #
+        # If top_level is False, it's because we're being called from
+        # one of the subcommand parsers, and we delegate to super.
+
+        if not top_level:
+            return super(WestArgumentParser, self).format_help()
+
+        # Format the help to be at most 75 columns wide, the maximum
+        # generally recommended by typographers for readability.
+        #
+        # If the terminal width (COLUMNS) is less than 75, use width
+        # (COLUMNS - 2) instead, unless that is less than 30 columns
+        # wide, which we treat as a hard minimum.
+        width = min(75, max(shutil.get_terminal_size().columns - 2, 30))
+
+        with StringIO() as sio:
+
+            def append(*strings):
+                for s in strings:
+                    print(s, file=sio)
+
+            append(self.format_usage(),
+                   self.description,
+                   '')
+
+            append('optional arguments:')
+            for wo in self.west_optionals:
+                self.format_west_optional(append, wo, width)
+
+            append('',
+                   'west commands used in various situations:')
+            for group, commands in COMMANDS.items():
+                if group is None:
+                    # Skip hidden commands.
+                    continue
+
+                append(' ' + group + ':')
+                for command in commands:
+                    self.format_command(append, command, width)
+                append('')
+
+            append(self.epilog)
+
+            return sio.getvalue()
+
+    def format_west_optional(self, append, wo, width):
+        metavar = wo['metavar']
+        options = wo['options']
+        help = wo.get('help')
+
+        # Join the various options together as a comma-separated list,
+        # with the metavar if there is one. That's our "thing".
+        if metavar is not None:
+            opt_str = '  ' + ', '.join('{} {}'.format(o, metavar)
+                                       for o in options)
+        else:
+            opt_str = '  ' + ', '.join(options)
+
+        # Delegate to the generic formatter.
+        self.format_thing_and_help(append, opt_str, help, width)
+
+    def format_command(self, append, command, width):
+        thing = '     {}:'.format(command.name)
+        self.format_thing_and_help(append, thing, command.help, width)
+
+    def format_thing_and_help(self, append, thing, help, width):
+        # Format help for some "thing" (arbitrary text) and its
+        # corresponding help text an argparse-like way.
+        help_offset = min(max(10, width - 20), 24)
+        help_indent = ' ' * help_offset
+
+        thinglen = len(thing)
+
+        if help is None:
+            # If there's no help string, just print the thing.
+            append(thing)
+        else:
+            # Reflow the lines in help to the desired with, using
+            # the help_offset as an initial indent.
+            help = ' '.join(help.split())
+            help_lines = textwrap.wrap(help, width=width,
+                                       initial_indent=help_indent,
+                                       subsequent_indent=help_indent)
+
+            if thinglen > help_offset - 1:
+                # If the "thing" (plus room for a space) is longer
+                # than the initial help offset, print it on its own
+                # line, followed by the help on subsequent lines.
+                append(thing)
+                append(*help_lines)
+            else:
+                # The "thing" is short enough that we can start
+                # printing help on the same line without overflowing
+                # the help offset, so combine the "thing" with the
+                # first line of help.
+                help_lines[0] = thing + help_lines[0][thinglen:]
+                append(*help_lines)
+
+    def add_argument(self, *args, **kwargs):
+        # Track information we want for formatting help.  The argparse
+        # module calls kwargs.pop(), so can't call super first without
+        # losing data.
+        optional = {'options': [], 'metavar': kwargs.get('metavar', None)}
+        need_metavar = (optional['metavar'] is None and
+                        kwargs.get('action') in (None, 'store'))
+        for arg in args:
+            if not arg.startswith('-'):
+                break
+            optional['options'].append(arg)
+            # If no metavar was given, the last option name is
+            # used. By convention, long options go last, so this
+            # matches the default argparse behavior.
+            if need_metavar:
+                optional['metavar'] = arg.lstrip('-').translate(
+                    {ord('-'): '_'}).upper()
+        optional['help'] = kwargs.get('help')
+        self.west_optionals.append(optional)
+
+        # Let argparse handle the actual argument.
+        super(WestArgumentParser, self).add_argument(*args, **kwargs)
 
 
 def command_handler(command, known_args, unknown_args):
@@ -156,12 +331,16 @@ def print_version_info():
 def parse_args(argv):
     # The prog='west' override avoids the absolute path of the main.py script
     # showing up when West is run via the wrapper
-    west_parser = argparse.ArgumentParser(
+    west_parser = WestArgumentParser(
         prog='west', description='The Zephyr RTOS meta-tool.',
-        epilog='Run "west <command> -h" for help on each command.')
+        epilog='Run "west <command> -h" for help on each command.',
+        add_help=False)
 
     # Remember to update scripts/west-completion.bash if you add or remove
     # flags
+
+    west_parser.add_argument('-h', '--help', action=WestHelpAction,
+                             help='show this help message and exit')
 
     west_parser.add_argument('-z', '--zephyr-base', default=None,
                              help='''Override the Zephyr base directory. The
@@ -172,12 +351,13 @@ def parse_args(argv):
                              help='''Display verbose output. May be given
                              multiple times to increase verbosity.''')
 
-    west_parser.add_argument('-V', '--version', action='store_true')
+    west_parser.add_argument('-V', '--version', action='store_true',
+                             help='print the program version and exit')
 
     subparser_gen = west_parser.add_subparsers(metavar='<command>',
                                                dest='command')
 
-    for command in COMMANDS:
+    for command in itertools.chain(*COMMANDS.values()):
         parser = command.add_parser(subparser_gen)
         parser.set_defaults(handler=partial(command_handler, command))
 
@@ -196,12 +376,7 @@ def parse_args(argv):
         set_zephyr_base(args)
 
     if 'handler' not in args:
-        if IN_MULTIREPO_INSTALL:
-            log.err('west installation found (in {}), but no command given'.
-                    format(west_dir()))
-        else:
-            log.err('no west command given')
-        west_parser.print_help(file=sys.stderr)
+        west_parser.print_help(file=sys.stderr, top_level=True)
         sys.exit(1)
 
     return args, unknown
