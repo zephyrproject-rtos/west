28cb52e4317a9725dea48317a6a3cf6bbbb9612d
Torsten Rasmussen <torsten.rasmussen@nordicsemi.no>
2019-02-08 12:06:46 +0100

    list: add posixpath, like abspath but with PurePath.as_posix()
    west list --format={abspath} is intended to use with Zephyr's CMake
    build system. In order for CMake not escape path, such as C:\<path>
    PurePath.as_posix() is used to ensure forward slashes when printing
    abspath in a new posixpath format key.
    
    Signed-off-by: Torsten Rasmussen <torsten.rasmussen@nordicsemi.no>
    Signed-off-by: Marti Bolivar <marti@foundries.io>

--- src/west/commands/project.py
+++ src/west/commands/project.py
@@ -123,6 +123,8 @@ class List(WestCommand):
             - path: the relative path to the project from the top level,
               as specified in the manifest where applicable
             - abspath: absolute and normalized path to the project
+            - posixpath: like abspath, but in posix style, that is, with '/'
+              as the separator character instead of '\\'
             - revision: project's manifest revision
             - cloned: "(cloned)" if the project has been cloned, "(not cloned)"
               otherwise
@@ -146,6 +148,7 @@ class List(WestCommand):
                     url=project.url,
                     path=project.path,
                     abspath=project.abspath,
+                    posixpath=project.posixpath,
                     revision=project.revision,
                     cloned="(cloned)" if _cloned(project) else "(not cloned)",
                     clone_depth=project.clone_depth or "None")
