dc031a63850ac0094241debd31c69143b5727d86
Marti Bolivar <marti.bolivar@nordicsemi.no>
2019-09-06 09:02:36 -0600

    main: fix west help <some-extension-command>
    This slipped through the cracks when adding the manifest field to each
    command instance in 7694f88 ("commands: save the parsed manifest from
    main"). It only shows up for extensions -- built-ins work fine.
    
    Add a regression test.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@nordicsemi.no>

--- tests/test_help.py
+++ tests/test_help.py
@@ -5,8 +5,9 @@
 from west.main import BUILTIN_COMMAND_NAMES
 from conftest import cmd
 
-def test_help_and_dash_h(west_init_tmpdir):
-    # Test "west help" and "west -h" are the same.
+def test_builtin_help_and_dash_h(west_init_tmpdir):
+    # Test "west help" and "west -h" are the same for built-in
+    # functionality.
 
     h1out = cmd('help')
     h2out = cmd('-h')
@@ -18,3 +19,22 @@ def test_help_and_dash_h(west_init_tmpdir):
         h1out = cmd('help {}'.format(c))
         h2out = cmd('{} -h'.format(c))
         assert h1out == h2out
+
+def test_extension_help_and_dash_h(west_init_tmpdir):
+    # Test "west help <command>" and "west <command> -h" for extension
+    # commands (west_init_tmpdir has a command with one).
+
+    cmd('update')
+    ext1out = cmd('help test-extension')
+    ext2out = cmd('test-extension -h')
+    assert ext1out == ext2out
+    assert ext1out == EXTENSION_EXPECTED
+
+
+EXTENSION_EXPECTED = '''\
+usage: west test-extension [-h]
+
+optional arguments:
+  -h, --help  show this help message and exit
+
+'''
