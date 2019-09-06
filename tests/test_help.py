ecb81be96bd7e4d99240f131bdad175617240d20
Marti Bolivar <marti.bolivar@nordicsemi.no>
2019-09-06 08:45:10 -0600

    tests: fix test_help
    There is a return that got in there that makes the test inoperative.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@nordicsemi.no>

--- tests/test_help.py
+++ tests/test_help.py
@@ -6,9 +6,7 @@ from west.main import BUILTIN_COMMAND_NAMES
 from conftest import cmd
 
 def test_help(west_init_tmpdir):
-
     # Check that output from help is equivalent to -h
-    return
 
     h1out = cmd('help')
     h2out = cmd('-h')
