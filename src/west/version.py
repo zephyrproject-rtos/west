9219704069fc4e9aace69aaa86ac4a5693d3bff9
Martí Bolívar <marti.bolivar@nordicsemi.no>
2020-02-06 07:03:13 -0800

    West 0.7.0
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- src/west/version.py
+++ src/west/version.py
@@ -5,4 +5,13 @@
 # This is the Python 3 version of option 3 in:
 # https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
 
-__version__ = '0.7.0rc2'
+__version__ = '0.7.0'
+# MAINTAINERS:
+#
+# Make sure to update west.manifest.SCHEMA_VERSION if there have been
+# manifest schema version changes since the last release.
+#
+# Note that this is the "logical" west manifest schema, and that the
+# pykwalify schema doesn't capture everything. E.g. the map in an
+# "import: <map>" is validated in west.manifest without pykwalify's
+# help.
