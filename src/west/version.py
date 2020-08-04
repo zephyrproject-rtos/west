520589fbe3fb516c945e5e16795db3c94838e429
Martí Bolívar <marti.bolivar@nordicsemi.no>
2020-08-04 13:38:00 -0700

    West v0.7.3
    The main purpose of this release is to get 877d46c ("app: fix import
    error followed by update") out into users' hands. We've only received
    one report of this happening in the wild so far, but it's an important
    enough bug to cut a point release for.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- src/west/version.py
+++ src/west/version.py
@@ -5,7 +5,7 @@
 # This is the Python 3 version of option 3 in:
 # https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
 
-__version__ = '0.7.2'
+__version__ = '0.7.3'
 # MAINTAINERS:
 #
 # Make sure to update west.manifest.SCHEMA_VERSION if there have been
