b6ecebaaf803a37547a2105908f8c37004c7d9a3
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-03-11 05:57:50 -0800

    West v0.9.1
    Bug fixes:
    
    - Commands like "west manifest --resolve" now correctly include group and
      group filter information.
    
    Other changes:
    
    - West now warns if you combine "import" with "group-filter". Semantics for
      this combination have changed starting with v0.10.x. See the v0.10.0 release
      notes for more information.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- src/west/version.py
+++ src/west/version.py
@@ -5,7 +5,7 @@
 # This is the Python 3 version of option 3 in:
 # https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
 
-__version__ = '0.9.1a1'
+__version__ = '0.9.1'
 # MAINTAINERS:
 #
 # Make sure to update west.manifest.SCHEMA_VERSION if there have been
