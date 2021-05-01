0009b22cf1daf6050dbe6c3e4b5bb4200b432176
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-04-30 17:59:49 -0700

    commands: fix git version detection on macOS
    I ran into this testing 0.11.0a1. Our current code matches against the
    last word in the 'git --version' output. Unfortunately, for 'git
    version 2.24.3 (Apple Git-128)', that's incorrectly matching against
    'Git-128)', which doesn't work.
    
    Generalize the version detection a bit to handle this, and add
    regression tests.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>
    (cherry picked from commit 4808c626e2638611ff29d35141d35784cdfc95da)

---
+++ tests/test_commands.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2021, Nordic Semiconductor ASA
+
+import os
+
+from west.commands import WestCommand
+
+assert 'TOXTEMPDIR' in os.environ, "you must run these tests using tox"
+
+gv = WestCommand._parse_git_version
+
+def test_parse_git_version():
+    # White box test for git parsing behavior.
+    assert gv(b'git version 2.25.1\n') == (2, 25, 1)
+    assert gv(b'git version 2.28.0.windows.1\n') == (2, 28, 0)
+    assert gv(b'git version 2.24.3 (Apple Git-128)\n') == (2, 24, 3)
+    assert gv(b'git version 2.29.GIT\n') == (2, 29)
