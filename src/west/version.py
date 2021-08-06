4b42f13250cc2948147deb242e91dcb393684e92
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-08-06 10:52:45 -0700

    West 0.11.1a1
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- src/west/version.py
+++ src/west/version.py
@@ -5,7 +5,7 @@
 # This is the Python 3 version of option 3 in:
 # https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
 
-__version__ = '0.11.0'
+__version__ = '0.11.1a1'
 #
 # MAINTAINERS:
 #
