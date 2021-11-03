65c001b125a058037c6a61713834f993f0064da0
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-11-03 11:23:08 -0700

    version: bump to v0.12.0a3
    This backports in the project userdata feature from main (which
    requires a manifest schema bump), along with reversion of the parallel
    west update feature for now.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- src/west/version.py
+++ src/west/version.py
@@ -5,12 +5,15 @@
 # This is the Python 3 version of option 3 in:
 # https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
 
-__version__ = '0.12.0a2'
+__version__ = '0.12.0a3'
 #
 # MAINTAINERS:
 #
-# Make sure to update west.manifest.SCHEMA_VERSION if there have been
-# manifest schema version changes since the last release.
+# - Make sure to update west.manifest.SCHEMA_VERSION if there have been
+#   manifest schema version changes since the last release.
+#
+# - If SCHEMA_VERSION has been updated since last release, make sure the
+#   above __version__ is in sync.
 #
 # Note that this is the "logical" west manifest schema, and that the
 # pykwalify schema doesn't capture everything. E.g. the map in an
