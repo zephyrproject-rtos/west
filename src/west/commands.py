zephyrproject-rtos/west

commit 2ac3e279ff98a4e62ed449430dfdf327881bff84

WestCommand: use Configuration objects
Add a 'config' property to WestCommand. This is a
west.configuration.Configuration accessible for the duration of
do_run(). For the same reason as the 'WestCommand.manifest' property
exists the way it does, we cannot add a new constructor argument to
capture the configuration. Handle this as a kwarg to run().

Take a Configuration in extension_commands().

Handle the necessary changes in main.py. These changes should be
invisible to all the extension commands I am aware of in the wild.

Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

src/west/commands.py
@@ -21,7 +21,7 @@ import pykwalify
 import yaml
 
 from west import log
-from west.configuration import config as _config
+from west.configuration import Configuration
 from west.manifest import Manifest
 from west.util import escapes_directory, quote_sh_list, PathType
 
@@ -103,9 +103,12 @@ class WestCommand(ABC):
         self.requires_workspace = requires_workspace
         self.topdir: Optional[str] = None
         self.manifest = None
+        self.config = None
 
     def run(self, args: argparse.Namespace, unknown: List[str],
-            topdir: PathType, manifest: Optional[Manifest] = None) -> None:
+            topdir: PathType,
+            manifest: Optional[Manifest] = None,
+            config: Optional[Configuration] = None) -> None:
         '''Run the command.
 
         This raises `west.commands.CommandContextError` if the command
@@ -119,6 +122,8 @@ class WestCommand(ABC):
             ``self.topdir`` from `WestCommand.do_run`
         :param manifest: `west.manifest.Manifest` or ``None``,
             accessible as ``self.manifest`` from `WestCommand.do_run`
+        :param config: `west.configuration.Configuration` or ``None``,
+            accessible as ``self.config`` from `WestCommand.do_run`
         '''
         if unknown and not self.accepts_unknown_args:
             self.parser.error(f'unexpected arguments: {unknown}')
@@ -126,6 +131,7 @@ class WestCommand(ABC):
             log.die(_no_topdir_msg(os.getcwd(), self.name))
         self.topdir = os.fspath(topdir) if topdir else None
         self.manifest = manifest
+        self.config = config
         self.do_run(args, unknown)
 
     def add_parser(self, parser_adder) -> argparse.ArgumentParser:
@@ -205,6 +211,28 @@ class WestCommand(ABC):
     # error from mypy by using this workaround:
     # https://github.com/python/mypy/issues/3004#issuecomment-726022329
     manifest = property(_get_manifest, _set_manifest)
+
+    @property
+    def has_config(self) -> bool:
+        '''Property which is True if self.config is safe to access.
+        '''
+        return self._config is not None
+
+    def _get_config(self) -> Configuration:
+        '''Property for the config which was passed to run().
+
+        If `do_run` was given a *config* kwarg, it is returned.
+        Otherwise, a fatal error occurs.
+        '''
+        if self._config is None:
+            log.die(f"can't run west {self.name}; it requires config "
+                    "variables, which were not available.")
+        return self._config
+
+    def _set_config(self, config: Optional[Configuration]):
+        self._config = config
+
+    config = property(_get_config, _set_config)
 
     #
     # Other public methods
@@ -338,7 +366,7 @@ class WestExtCommandSpec:
                 f' help={repr(self.help)}'
                 f' factory={self.factory}>')
 
-def extension_commands(manifest=None):
+def extension_commands(config: Configuration, manifest: Manifest = None):
     # Get descriptions of available extension commands.
     #
     # The return value is an ordered map from project paths to lists of
@@ -352,8 +380,8 @@ def extension_commands(manifest=None):
     # :param manifest: a parsed ``west.manifest.Manifest`` object, or None
     #                  to reload a new one.
 
-    allow_extensions = _config.getboolean('commands', 'allow_extensions',
-                                          fallback=True)
+    allow_extensions = config.getboolean('commands.allow_extensions',
+                                         default=True)
     if not allow_extensions:
         return {}
 

src/west/app/main.py
@@ -221,7 +221,7 @@ class WestApp:
             self.extensions = None
             return
 
-        path_specs = extension_commands(manifest=self.manifest)
+        path_specs = extension_commands(self.config, manifest=self.manifest)
         extension_names = set()
 
         for path, specs in path_specs.items():
@@ -336,7 +336,8 @@ class WestApp:
                     self.handle_builtin_manifest_load_err(args)
 
                 cmd = self.builtins.get(args.command, self.builtins['help'])
-                cmd.run(args, unknown, self.topdir, manifest=self.manifest)
+                cmd.run(args, unknown, self.topdir, manifest=self.manifest,
+                        config=self.config)
             else:
                 self.run_extension(args.command, argv)
         except KeyboardInterrupt:
@@ -410,7 +411,8 @@ class WestApp:
         #   themselves (easy if above is OK, unnecessary if it isn't)
         set_zephyr_base(args, self.manifest, self.topdir, self.config)
 
-        command.run(args, unknown, self.topdir, manifest=self.manifest)
+        command.run(args, unknown, self.topdir, manifest=self.manifest,
+                    config=self.config)
 
 class Help(WestCommand):
     # west help <command> implementation.

commit dbf6d63ce2b1f5ff79777bfe4fe272e15a82fc42

commands: fix "no workspace" and other error paths
Commit 2ac3e279ff98a4e62ed449430dfdf327881bff84
("WestCommand: use Configuration objects") introduced a
configuration attribute to WestCommand instances, and used
it to look up configuration options.

However, it stored this attribute at the wrong time in
WestCommand.run(). We need to stash the configuration before doing
anything else, because otherwise calls like self.die() in the same
method will look up self.color_ui, which looks up the configuration,
which isn't present, which calls self.die(), leading to an infinite
recursion.

If we stash the configuration immediately, we instead have a valid
configuration option and we get an error printed instead.

Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

src/west/commands.py
@@ -182,13 +182,13 @@ class WestCommand(ABC):
         :param config: `west.configuration.Configuration` or ``None``,
             accessible as ``self.config`` from `WestCommand.do_run`
         '''
+        self.config = config
         if unknown and not self.accepts_unknown_args:
             self.parser.error(f'unexpected arguments: {unknown}')
         if not topdir and self.requires_workspace:
             self.die(_no_topdir_msg(os.getcwd(), self.name))
         self.topdir = os.fspath(topdir) if topdir else None
         self.manifest = manifest
-        self.config = config
         for hook in self._hooks:
             hook(self)
         self.do_run(args, unknown)