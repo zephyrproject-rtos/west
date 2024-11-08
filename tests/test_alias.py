34f35f6748b29ccca3dbe8be96809edc2fd9d3cb
Pieter De Gendt <pieter.degendt@basalte.be>
2024-11-08 14:56:28 +0100

    tests: Format test_alias.py
    Formatting test_alias.py is trivial.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- tests/test_alias.py
+++ tests/test_alias.py
@@ -10,12 +10,14 @@ from conftest import cmd
 
 assert 'TOXTEMPDIR' in os.environ, "you must run these tests using tox"
 
+
 @pytest.fixture(autouse=True)
 def autouse_tmpdir(config_tmpdir, west_init_tmpdir):
     # Since this module tests west's configuration file features,
     # adding autouse=True to the config_tmpdir and west_init_tmpdir fixtures
     # saves typing and is less error-prone than using it below in every test case.
     pass
+
 
 def test_alias_commands():
     cmd('config alias.test1 topdir')
@@ -28,6 +30,7 @@ def test_alias_commands():
     assert cmd('test2') == topdir_out
     assert cmd('test3') == topdir_out
 
+
 def test_alias_help():
     cmd('config alias.test topdir')
 
@@ -36,12 +39,14 @@ def test_alias_help():
     assert "An alias that expands to: topdir" in help_out
     assert cmd('-h test') == help_out
 
+
 def test_alias_recursive_commands():
     list_format = '{revision} TESTALIAS {name}'
     cmd(['config', 'alias.test1', f'list -f "{list_format}"'])
     cmd('config alias.test2 test1')
 
     assert cmd('test2') == cmd(['list', '-f', list_format])
+
 
 def test_alias_infinite_recursion():
     cmd('config alias.test1 test2')
@@ -52,6 +57,7 @@ def test_alias_infinite_recursion():
         cmd('test1', stderr=subprocess.STDOUT)
 
     assert 'unknown command "test1";' in str(excinfo.value.stdout)
+
 
 def test_alias_empty():
     cmd(['config', 'alias.empty', ''])
@@ -64,17 +70,20 @@ def test_alias_empty():
 
     assert 'empty alias "empty"' in str(excinfo.value.stdout)
 
+
 def test_alias_early_args():
     cmd('config alias.test1 topdir')
 
     # An alias with an early command argument shouldn't fail
     assert "Replacing alias test1 with ['topdir']" in cmd('-v test1')
 
+
 def test_alias_command_with_arguments():
     list_format = '{revision} TESTALIAS {name}'
     cmd(['config', 'alias.revs', f'list -f "{list_format}"'])
 
     assert cmd('revs') == cmd(['list', '-f', list_format])
+
 
 def test_alias_override():
     before = cmd('list')
