93886706b0b8a0f6b9679750694843ff5a34df0b
Klein, Thorsten <Thorsten.Klein@bshg.com>
2025-07-24 09:28:44 +0200

    extend existing caching tests
    ´Existing tests for --name-cache and --path-cache are extended.
    Additional comments are added and the correct remote url is asserted for
    each cloned repository in the workspace. In order to ensure unique
    commit ids in the tests, individual commit messages (using uuid.uuid4)
    are used within the tests when a repo is created.
    
    Signed-off-by: Thorsten Klein <thorsten.klein@bshg.com>

--- tests/test_project_caching.py
+++ tests/test_project_caching.py
@@ -2,22 +2,33 @@
 
 import os
 import shutil
+from pathlib import Path
 
 from conftest import (
     cmd,
     create_repo,
     create_workspace,
+    remote_get_url,
     rev_parse,
 )
 
 assert 'TOXTEMPDIR' in os.environ, "you must run these tests using tox"
 
+#
+# Helpers
+#
 
-def setup_cache_workspace(workspace, remote, foo_head, bar_head):
+def setup_cache_workspace(workspace, foo_remote, foo_head, bar_remote, bar_head):
     # Shared helper code that sets up a workspace used to test the
     # 'west update --foo-cache' options.
 
     create_workspace(workspace)
+
+    # The directory tree looks like following:
+    # (workspace)
+    # ├── bar
+    # └── subdir
+    #     └── foo
 
     manifest_project = workspace / 'mp'
     with open(manifest_project / 'west.yml', 'w') as f:
@@ -26,31 +37,47 @@ def setup_cache_workspace(workspace, remote, foo_head, bar_head):
           projects:
           - name: foo
             path: subdir/foo
-            url: file://{remote}
+            url: file://{foo_remote}
             revision: {foo_head}
           - name: bar
-            url: file://{remote}
+            url: file://{bar_remote}
             revision: {bar_head}
         ''')
 
+#
+# Test cases
+#
 
 def test_update_name_cache(tmpdir):
     # Test that 'west update --name-cache' works and doesn't hit the
     # network if it doesn't have to.
 
-    remote = tmpdir / 'remote'
-    create_repo(remote)
+    # The directory tree of the name cache looks like following:
+    # (name cache)
+    # ├── bar
+    # └── foo
+
     name_cache_dir = tmpdir / 'name_cache'
     create_repo(name_cache_dir / 'foo')
     create_repo(name_cache_dir / 'bar')
     foo_head = rev_parse(name_cache_dir / 'foo', 'HEAD')
     bar_head = rev_parse(name_cache_dir / 'bar', 'HEAD')
 
+    # setup the workspace (remote url can be non-existent, since the repository
+    # should be cloned from local cache)
     workspace = tmpdir / 'workspace'
-    setup_cache_workspace(workspace, remote, foo_head, bar_head)
+    setup_cache_workspace(workspace,
+                          foo_remote=(Path('non-existent') / 'here'),
+                          foo_head=foo_head,
+                          bar_remote=(Path('non-existent') / 'there'),
+                          bar_head=bar_head)
     workspace.chdir()
     foo = workspace / 'subdir' / 'foo'
     bar = workspace / 'bar'
+
+    # Test that foo and bar are created within the workspace. They must be
+    # checked out at correct commit id and their remote url is set to original
+    # remote url (not local cache path).
 
     # Test the command line option.
     cmd(['update', '--name-cache', name_cache_dir])
@@ -58,6 +85,8 @@ def test_update_name_cache(tmpdir):
     assert bar.check(dir=1)
     assert rev_parse(foo, 'HEAD') == foo_head
     assert rev_parse(bar, 'HEAD') == bar_head
+    assert remote_get_url(foo) == "file://" + os.fspath(Path('non-existent') / 'here')
+    assert remote_get_url(bar) == "file://" + os.fspath(Path('non-existent') / 'there')
 
     # Move the repositories out of the way and test the configuration option.
     # (We can't use shutil.rmtree here because Windows.)
@@ -69,25 +98,42 @@ def test_update_name_cache(tmpdir):
     assert bar.check(dir=1)
     assert rev_parse(foo, 'HEAD') == foo_head
     assert rev_parse(bar, 'HEAD') == bar_head
+    assert remote_get_url(foo) == "file://" + os.fspath(Path('non-existent') / 'here')
+    assert remote_get_url(bar) == "file://" + os.fspath(Path('non-existent') / 'there')
 
 
 def test_update_path_cache(tmpdir):
     # Test that 'west update --path-cache' works and doesn't hit the
     # network if it doesn't have to.
+    # Note: Remote url can be non-existent since it will clone from local cache
 
-    remote = tmpdir / 'remote'
-    create_repo(remote)
+    # The directory tree of the name cache looks like following:
+    # (path cache)
+    # ├── bar
+    # └── subdir
+    #     └── foo
+
     path_cache_dir = tmpdir / 'path_cache_dir'
     create_repo(path_cache_dir / 'subdir' / 'foo')
     create_repo(path_cache_dir / 'bar')
     foo_head = rev_parse(path_cache_dir / 'subdir' / 'foo', 'HEAD')
     bar_head = rev_parse(path_cache_dir / 'bar', 'HEAD')
 
+    # setup the workspace (remote url can be non-existent, since the repository
+    # should be cloned from local cache)
     workspace = tmpdir / 'workspace'
-    setup_cache_workspace(workspace, remote, foo_head, bar_head)
+    setup_cache_workspace(workspace,
+                          foo_remote=(Path('non-existent') / 'here'),
+                          foo_head=foo_head,
+                          bar_remote=(Path('non-existent') / 'there'),
+                          bar_head=bar_head)
     workspace.chdir()
     foo = workspace / 'subdir' / 'foo'
     bar = workspace / 'bar'
+
+    # Test that foo and bar are created within the workspace. They must be
+    # checked out at correct commit id and their remote url is set to original
+    # remote url (not local cache path).
 
     # Test the command line option.
     cmd(['update', '--path-cache', path_cache_dir])
@@ -95,6 +141,8 @@ def test_update_path_cache(tmpdir):
     assert bar.check(dir=1)
     assert rev_parse(foo, 'HEAD') == foo_head
     assert rev_parse(bar, 'HEAD') == bar_head
+    assert remote_get_url(foo) == "file://" + os.fspath(Path('non-existent') / 'here')
+    assert remote_get_url(bar) == "file://" + os.fspath(Path('non-existent') / 'there')
 
     # Move the repositories out of the way and test the configuration option.
     # (We can't use shutil.rmtree here because Windows.)
@@ -106,3 +154,5 @@ def test_update_path_cache(tmpdir):
     assert bar.check(dir=1)
     assert rev_parse(foo, 'HEAD') == foo_head
     assert rev_parse(bar, 'HEAD') == bar_head
+    assert remote_get_url(foo) == "file://" + os.fspath(Path('non-existent') / 'here')
+    assert remote_get_url(bar) == "file://" + os.fspath(Path('non-existent') / 'there')
