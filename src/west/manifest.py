zephyrproject-rtos/west

commit 2b2d3f2685e688974690c66dfeb1c8d878c071fd

manifest: Do not allow projects inside the west directory
It's not allowed to have projects inside the .west directory,
add a check when parsing the manifest.

Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

src/west/manifest.py
@@ -2435,6 +2435,10 @@ class Manifest:
                             f'normalizes to {ret_norm}, which escapes '
                             f'the workspace topdir')
 
+        if Path(ret_norm).parts[0] == util.WEST_DIR:
+            self._malformed(f'project "{name}" path {ret.path} '
+                            f'is in the {util.WEST_DIR} directory')
+
         return ret
 
     def _validate_project_groups(self, project_name: str,

commit 007755c4e5c16d54207e3fb840fe793033d9be3c

manifest: restore "path: ." ability but with a warning
Restore the ability to have a project at the top level but with an
"unsupported" warning printed every time.

Fixes v1.3 commit 2b2d3f2685e6 ("manifest: Do not allow projects inside
the west directory") which accidentally broke it with an pretty obscure
backtrace:

File "python/site-packages/west/manifest.py", line 2460, in _load_project
    if Path(ret_norm).parts[0] == util.WEST_DIR:
       ~~~~~~~~~~~~~~~~~~~~^^^
IndexError: tuple index out of range

Fixes issue number #910

This is STILL not supported!

Signed-off-by: Marc Herbert <marc.herbert@intel.com>

src/west/manifest.py
@@ -2534,7 +2534,19 @@ class Manifest:
                 f'the workspace topdir'
             )
 
-        if Path(ret_norm).parts[0] == util.WEST_DIR:
+        _path = Path(ret_norm)
+
+        # Unsupported, see https://github.com/zephyrproject-rtos/zephyr/commit/7d40091fbfedfc
+        if _path == Path('.'):
+            _logger.warning("Making the topdir a git clone is not supported; do not report bugs.")
+            return ret
+
+        # Can any other Path have zero part? Just in case, provide a decent error message
+        # instead of "index out of range" (see #910)
+        if len(_path.parts) < 1:
+            self._malformed(f'project "{name}" path "{ret.path}" has no part?! ({_path})')
+
+        if _path.parts[0] == util.WEST_DIR:
             self._malformed(
                 f'project "{name}" path "{ret.path}" is in the {util.WEST_DIR} directory'
             )