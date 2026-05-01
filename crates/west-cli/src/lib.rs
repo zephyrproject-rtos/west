57e77dba0c337f8bc71f8334a20b3b3ab372db41
Pieter De Gendt <pieter.degendt@basalte.be>
2026-05-01 19:54:04 +0200

    crates: Add clap_verbosity_flag for log level
    Use the crate to setup the log level.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- crates/west-cli/src/lib.rs
+++ crates/west-cli/src/lib.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 use std::process::ExitCode;
 
 use clap::Parser;
+use clap_verbosity_flag::Verbosity;
 
 pub mod commands;
 
@@ -12,12 +13,19 @@ pub struct Cli {
     #[arg(short = 'C', value_name = "DIR", global = true)]
     pub chdir: Option<PathBuf>,
 
+    #[command(flatten)]
+    pub verbosity: Verbosity,
+
     #[command(subcommand)]
     pub command: commands::Command,
 }
 
 pub fn run() -> ExitCode {
     let cli = Cli::parse();
+
+    env_logger::Builder::new()
+        .filter_level(cli.verbosity.log_level_filter())
+        .init();
 
     if let Some(dir) = &cli.chdir {
         if let Err(e) = std::env::set_current_dir(dir) {
